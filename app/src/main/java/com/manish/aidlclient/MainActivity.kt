cfe235c7f4969c8f8c5a7e156cfa30cfb102a5da
Manish Mishra <mishramanish426@gmail.com>
2023-12-24 12:30:35 +0530

    Add Click and IColorAidlInterface and functionality

--- app/src/main/java/com/manish/aidlclient/MainActivity.kt
+++ app/src/main/java/com/manish/aidlclient/MainActivity.kt
@@ -1,15 +1,55 @@
 package com.manish.aidlclient
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.graphics.Color
 import android.os.Bundle
+import android.os.IBinder
+import android.os.RemoteException
+import androidx.appcompat.app.AppCompatActivity
 import com.manish.aidlclient.databinding.ActivityMainBinding
+import com.manish.aidlserver.IColorAidlInterface
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    private var iColorAidlInterface: IColorAidlInterface? = null
+
+    private val serviceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, binder: IBinder?) {
+            iColorAidlInterface = IColorAidlInterface.Stub.asInterface(binder)
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            iColorAidlInterface = null
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        checkAidlColor()
+        clickHandler()
     }
+
+    private fun clickHandler() {
+        binding.aidlServiceCall.setOnClickListener {
+            try {
+                binding.aidlServiceCall.setBackgroundColor(iColorAidlInterface?.color ?: Color.RED)
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun checkAidlColor() {
+        val intent = Intent("AIDLColorService")
+        intent.setPackage("com.manish.aidlserver")
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE)
+    }
+
+
 }
